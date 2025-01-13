c5facd1a24b2dacec64f45a0d7a98a2fc8b30f79
Sadhana Thirumangai Kalidoss <147619061+sthirkal@users.noreply.github.com>
2025-01-13 11:17:52 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
-export const SITE_TITLE = "Miniblog";
+export const SITE_URL = "https://sthirkal.github.io";
+export const SITE_TITLE = "sthirkal's void";
 export const SITE_DESCRIPTION =
-  "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
+  "thoughts & prayers";
 
-export const EMAIL = "hello@nicholasly.com";
+export const EMAIL = "sthirkal@gmail.com";
