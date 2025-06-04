3b78c61c4c238257840494f6f9388ea748267241
thennal10 <thennal10@gmail.com>
2025-06-04 23:11:51 +0530

    Switch sthirkal to vierresc

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://sthirkal.github.io";
-export const SITE_TITLE = "sthirkal's void";
+export const SITE_URL = "https://vierresc.github.io";
+export const SITE_TITLE = "vierresc's void";
 export const SITE_DESCRIPTION =
   "thoughts & prayers";
 
